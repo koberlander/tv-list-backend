5c478992ff3cd139d9f0a4398a2f0b7c3d2dfd1f
github username <github email address>
2019-07-25 13:25:44 -0400

    created seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# do that thing where it deletes all olds before loading so that you don't create multiples
+
+
+
+
+# Seed a few shows and initialize with favorite set to false
+# Program.create!(url: '', name: '', network: '', image: '', favorite: false)
+
+Program.create!(url: 'https://www.tvmaze.com/shows/2/person-of-interest', name: 'Person of Interest', network: 'CBS', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/163/407679.jpg', watchlist: true)
+Program.create!(url: 'https://www.tvmaze.com/shows/5/true-detective', name: 'True Detective', network: 'HBO', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg', watchlist: false)
+Program.create!(url: 'https://www.tvmaze.com/shows/40/death-note', name: 'Death Note', network: 'NTV', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/0/361.jpg', watchlist: false)
