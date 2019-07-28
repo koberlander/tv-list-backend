f3bc94e13c434e962a7e9bc4e797aa7fdc6a207b
github username <github email address>
2019-07-28 11:10:24 -0400

    tried to fix fetch route error, but still get an error on my show method

--- app/controllers/api/v1/programs_controller.rb
+++ app/controllers/api/v1/programs_controller.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 # added namespacing here as well to coincide with our routes
 class Api::V1::ProgramsController < ApplicationController
 
   def index
     @programs = Program.all
+    pry
     render json: @programs
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
-Rails.application.routes.draw do
 # namespace each folder we add for a fetch request to http://localhost:3000/api/v1/programs
+# we nest our routes so that we can view the relationship between one program and all of its comments. I removed access to api/v1/comments because that doesn't make sense to have in this app.
+Rails.application.routes.draw do
   namespace :api do
 
     namespace :v1 do
-      # we nest our routes so that we can view the relationship between one program and all of its comments. I removed access to api/v1/comments because that doesn't make sense to have in this app.
+
       resource :programs do
-        resource :comments
+
+          resource :comments
+          
       end
 
     end
