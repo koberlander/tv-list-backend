e284f119e3ca57ffd9f174d103a1713518394305
github username <github email address>
2019-07-29 09:51:18 -0400

    fleshing out seed data with current shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 # do that thing where it deletes all olds before loading so that you don't create multiples
 
-
-
-
 # Seed a few shows and initialize with favorite set to false
-# Program.create!(url: '', name: '', network: '', image: '', favorite: false)
+# Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
 
-poi = Program.create!(url: 'https://www.tvmaze.com/shows/2/person-of-interest', name: 'Person of Interest', network: 'CBS', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/163/407679.jpg', watchlist: true)
+russian_doll = Program.create!(url: 'http://www.tvmaze.com/shows/39987/russian-doll', name: 'Russian Doll', network: 'Netflix', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/179/448863.jpg', watchlist: false)
 
-td = Program.create!(url: 'https://www.tvmaze.com/shows/5/true-detective', name: 'True Detective', network: 'HBO', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg', watchlist: false)
+nine_nine = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
 
-dn = Program.create!(url: 'https://www.tvmaze.com/shows/40/death-note', name: 'Death Note', network: 'NTV', image: 'http://static.tvmaze.com/uploads/images/medium_portrait/0/361.jpg', watchlist: false)
+paradise_pd = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+american_gods = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+jessica_jones = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+schitts_creek = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+better_call_saul = Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
+
+Program.create!(url: '', name: '', network: '', image: '', watchlist: false)
